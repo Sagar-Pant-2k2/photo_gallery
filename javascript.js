dc7822813f71e394ecf67b6775fd98d4fbc39a6f
Sagar-Pant-2k2 <108977152+Sagar-Pant-2k2@users.noreply.github.com>
2023-01-03 16:19:01 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,5 @@
+// overlay=document.querySelector('.overlay');
+document.querySelector('.continue').addEventListener('click',function(){
+    document.querySelector('.overlay').classList.add("hidden");
+});
+console.log("working fine");
